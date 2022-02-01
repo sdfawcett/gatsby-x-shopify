91a8e16524a724d5cbad632fdf630b74a41408a3
Shayne <shayne.fawcett@gmail.com>
2022-02-01 15:35:26 -0500

    added image gallery to product page

--- src/templates/ProductTemplate/index.js
+++ src/templates/ProductTemplate/index.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { Layout, ImageGallery } from 'components';
+import { Grid } from './styles';
 
 export const query = graphql`
     query ProductQuery($shopifyId: String){
         shopifyProduct(shopifyId: { eq: $shopifyId }) {
             title
-          }
-    }
+            description
+                images {
+                    id
+                    localFile {
+                        childImageSharp {
+                            fluid(maxWidth: 300) {
+                                ...GatsbyImageSharpFluid_withWebp
+                            }
+                        }
+                    }
+                }
+            }
+        }
 `;
 
 export default function ProductTemplate(props) {
-    console.log(props);
-    return <h1>{props.data.shopifyProduct.title}</h1>;
+    return (
+        <Layout>
+            <Grid>
+                <div>
+                    <h1>{props.data.shopifyProduct.title}</h1>
+                    <p>{props.data.shopifyProduct.description}</p>
+                </div>
+                <div>
+                    <ImageGallery images={props.data.shopifyProduct.images} />
+                </div>
+            </Grid>
+        </Layout>
+    );
 }
