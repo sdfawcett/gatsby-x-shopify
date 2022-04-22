78e43a7080cc707789b308d20daaba9eab115c1d
Shayne <shayne.fawcett@gmail.com>
2022-04-22 12:33:01 -0400

    put new hero with video bg on index page

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { LayoutWrapper } from './styles';
 import { Header } from '../Header';
+import Hero from '../Hero';
 
 const Layout = ({ children }) => {
   return (
     <>
     <Header />
+    <Hero />
       <LayoutWrapper>
         <main>{children}</main>
       </LayoutWrapper>
